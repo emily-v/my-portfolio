c4ee1fbd42b0a6efa1f44817cd5cf7ec53b42111
Emily Vong <emily.vong25@gmail.com>
2018-08-03 14:39:53 +0000

    finished navbar and jumbotron layouts

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -7,7 +7,37 @@ import PropTypes from 'prop-types';
 function Navbar(props){
     return (
         <div>
-            Test
+            <header className="bg-light">
+                <div className="row py-3">
+                    <div className="col-md-6 d-flex justify-content-center justify-content-md-start">
+                        <h1 className="pl-md-3">Emily Vong</h1>
+                    </div>
+                    <div className="col d-flex justify-content-center justify-content-md-end align-items-end">
+                        <h3 className="pr-md-3">Full-Stack Web Developer</h3>
+                    </div>
+                </div>
+            </header>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-secondary navbar-toggleable-md">
+                <button className="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarToggleContainer">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarToggleContainer">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item active mx-3">
+                            <Link to="#projects" className="nav-link">Projects</Link>
+                        </li>
+                        <li className="nav-item active mx-3">
+                            <Link to="#about" className="nav-link" href="#">About</Link>
+                        </li>
+                        <li className="nav-item active mx-3">
+                            <Link to="#contact" className="nav-link" href="#">Contact</Link>
+                        </li>
+                        <li className="nav-item active mx-3">
+                            <Link to="#resume" className="nav-link" href="#">Resume</Link>{/*make this link to open/download resume in new tab*/}
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -11,10 +11,21 @@ export class Home extends React.Component{
     render(){
         return (
             <React.Fragment>
-                <Navbar />
-                <div className="text-center mt-5">
-                    <h1>Hello World!</h1>
-                    <a href="#" className="btn btn-success">Yay Bootstrap!</a>
+                <div className="container">
+                    <Navbar />
+                    {/*-----BEGIN JUMBOTRON-----*/}
+                    <div className="jumbotron">
+                        <div className="row">
+                            <div className="col-md-6 text-center">
+                                <img src="http://via.placeholder.com/600x450" alt="hero" className="img-fluid"/>
+                            </div>
+                            <div className="col px-sm-3">
+                                <p className="lead pt-3 pt-md-0">
+                                    <span className="h4">Greetings!</span> Lorem ipsum dolor amet activated charcoal laboris mollit mlkshk. Gastropub distillery incididunt twee paleo af nulla forage. Please view my projects and feel free to contact me with feedback, questions or interest!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
